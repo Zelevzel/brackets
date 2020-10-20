9dcad2350fa23608fecead74a44f0ca902d1e2ac
Evgeniy <zelevzel@ya.ru>
2020-10-20 15:28:06 +0300

    feat: init task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    const stack = [];
+    let indexOpen,
+        indexClose,
+        openBracket;
+
+    for (char of str) {
+
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            for (let j = 0; j < 2; j++) {
+
+                openBracket = stack[stack.length - 1]
+                if ((bracketsConfig[i][j] === openBracket) && (j === 0)) {
+                    indexOpen = i;
+                }
+
+                if ((bracketsConfig[i][j] === char) && (j === 0)) {
+                    stack.push(char);
+                    indexOpen = i;
+                    continue;
+
+                } else {
+                    if ((bracketsConfig[i][j] === char) && (j === 1)) {
+                        indexClose = i;
+
+                        if (indexOpen === indexClose) {
+                            stack.pop();
+                            continue;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+
+    return stack.length === 0;
+
 }
